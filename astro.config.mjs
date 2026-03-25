94a9e3292f93296723b8da8ea2b3f7b875b5a19c
alex-rublevsky <alexander.rublevskii@gmail.com>
2026-03-25 01:19:28 -0400

    here we go again...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,34 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    //TODO:
+	//site: "https://rublevsky.pages.dev/",
+	output: "static",
+	prefetch: {
+		defaultStrategy: "viewport",
+	},
+	integrations: [sitemap(), robotsTxt()],
+	// image: {
+	// 	domains: ["assets.rublevsky.com"],
+	// },
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": "/src",
+			},
+		},
+		// build: {
+		//   // Enable aggressive chunking for better caching
+		//   rollupOptions: {
+		//     output: {
+		//       manualChunks: undefined,
+		//     },
+		//   },
+		//},
+	},
+});
